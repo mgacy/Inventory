8103f6ea7ad320db90d5e02c51748d5d59a8915e
mgacy <matt@mgacy.com>
2016-11-03 12:24:15 -0700

    Add extensions for Vendor and Unit entities
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ Mobile/Vendor+MyStuff.swift
@@ -0,0 +1,31 @@
+//
+//  Vendor+MyStuff.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 11/2/16.
+//  Copyright © 2016 Mathew Gacy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import SwiftyJSON
+
+extension Vendor {
+    
+    // MARK: - Lifecycle
+    
+    convenience init(context: NSManagedObjectContext, json: JSON) {
+        self.init(context: context)
+        
+        if let remoteID = json["id"].int {
+            self.remoteID = Int32(remoteID)
+        }
+        if let name = json["name"].string {
+            self.name = name
+        }
+
+        // TODO - create separate VendorRep object
+        
+    }
+    
+}
