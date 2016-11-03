146274f5906397a8ec191e88b8fdbfb656b474cc
mgacy <matt@mgacy.com>
2016-11-03 12:23:36 -0700

    Remove unused code
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/InventoryKeypadVC.swift
+++ Mobile/InventoryKeypadVC.swift
@@ -65,8 +65,6 @@ class InventoryKeypadVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //updateForNewItem()
-        // NEW
         update(newItem: true)
     }
     
@@ -77,24 +75,13 @@ class InventoryKeypadVC: UIViewController {
     
     // MARK: - Keypad
     
-    /*
-     TODO: should any call to keypad return (quantity: Double, history: String) so
-     that those can then be passed both to currentItem and the display?
-     */
-    
     @IBAction func numberTapped(_ sender: AnyObject) {
         guard let digit = sender.currentTitle else { return }
         print("Tapped '\(digit)'")
         guard let number = Int(digit!) else { return }
         keypad.pushDigit(value: number)
         
-        // Update model with result of keypad
-        //updateModel()
-        
-        // Update display with updated model properties
-        //updateDisplay()
-        
-        // NEW
+        // Update model and display with result of keypad
         update()
     }
     
@@ -102,13 +89,7 @@ class InventoryKeypadVC: UIViewController {
         print("Tapped 'clear'")
         keypad.popItem()
         
-        // Update model with result of keypad
-        //updateModel()
-        
-        // Update display with updated model properties
-        //updateDisplay()
-        
-        // NEW
+        // Update model and display with result of keypad
         update()
     }
     
@@ -116,13 +97,7 @@ class InventoryKeypadVC: UIViewController {
         print("Tapped '.'")
         keypad.pushDecimal()
         
-        // Update model with result of keypad
-        //updateModel()
-        
-        // Update display with updated model properties
-        //updateDisplay()
-        
-        // NEW
+        // Update model and display with result of keypad
         update()
     }
     
@@ -132,13 +107,7 @@ class InventoryKeypadVC: UIViewController {
         print("Tapped '+'")
         keypad.pushOperator()
         
-        // Update model with result of keypad
-        //updateModel()
-        
-        // Update display with updated model properties
-        //updateDisplay()
-        
-        // NEW
+        // Update model and display with result of keypad
         update()
     }
     
@@ -152,13 +121,7 @@ class InventoryKeypadVC: UIViewController {
         keypad.pushDigit(value: 1)
         keypad.pushOperator()
         
-        // Update model with result of keypad
-        //updateModel()
-        
-        // Update display with updated model properties
-        //updateDisplay()
-        
-        // NEW
+        // Update model and display with result of keypad
         update()
     }
     
@@ -168,9 +131,7 @@ class InventoryKeypadVC: UIViewController {
         if currentIndex < items.count - 1 {
             currentIndex += 1
             
-            //updateForNewItem()
-            
-            // NEW
+            // Update keypad and display with new currentItem
             update(newItem: true)
             
         } else {
@@ -184,10 +145,8 @@ class InventoryKeypadVC: UIViewController {
     @IBAction func previousItemTapped(_ sender: AnyObject) {
         if currentIndex > 0 {
             currentIndex -= 1
-
-            //updateForNewItem()
             
-            // NEW
+            // Update keypad and display with new currentItem
             update(newItem: true)
             
         } else {
@@ -199,74 +158,8 @@ class InventoryKeypadVC: UIViewController {
     }
     
     // MARK: - C
-    
-    func updateModel() {
-        if let keypadResult = keypad.evaluateHistory() {
-            currentItem.quantity = keypadResult as NSNumber?
-        } else {
-            currentItem.quantity = nil
-        }
         
-        // Save the context.
-        let context = self.managedObjectContext!
-        do {
-            try context.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-    }
-    
-    // MARK: NEW
-    /*
-    func updateDisplay() {
-        let output = keypad.output()
-        print("Output: \(output)")
-        
-        // Item.quantity
-        itemValue.text = output.display
-        if output.total != nil {
-            itemValue.textColor = UIColor.black
-        } else {
-            itemValue.textColor = UIColor.lightGray
-        }
-        
-        itemHistory.text = output.history
-        
-        // Item.name
-        guard let item = currentItem.item else {
-            itemName.text = "Error (1)"
-            return
-        }
-        guard let name = item.name else {
-            itemName.text = "Error (2)"
-            return
-        }
-        itemName.text = name
-        
-        // Item.pack
-        
-        // Item.unit
-    }
-    
-    func updateForNewItem() {
-        // print("updateForNewItem: \(currentItem.item?.name) - \(currentItem)")
-        
-        // Update keypad with info from new currentItem
-        keypad.updateNumber(currentItem.quantity as Double?)
-        
-        // Update display
-        updateDisplay()
-    }
-    */
-    // MARK: - NEW / CONSOLIDATED
-    
-    // NEW - Consolidate updateDisplay, updateModel and updateForNewItem
     func update(newItem: Bool = false) {
-        
-        //let output: (history: String, total: Double?, display: String)
         let output: keypadOutput
         
         switch newItem {
@@ -296,73 +189,10 @@ class InventoryKeypadVC: UIViewController {
             }
         }
         
-        // Update Display
-        /*
-        updateDisplay(item: currentItem, history: output.history, total: output.total,
-                      display: output.display)
-        */
         updateDisplay(item: currentItem, keypadOutput: output)
-        /*
-        // TODO - Should I reencapsulate this in a method?
-        
-        // Item.quantity
-        itemValue.text = output.display
-        if output.total != nil {
-            itemValue.textColor = UIColor.black
-        } else {
-            itemValue.textColor = UIColor.lightGray
-        }
-        
-        itemHistory.text = output.history
-        
-        // Item.name
-        guard let item = currentItem.item else {
-            itemName.text = "Error (1)"
-            return
-        }
-        guard let name = item.name else {
-            itemName.text = "Error (2)"
-            return
-        }
-        itemName.text = name
-        
-        // Item.pack
-        
-        // Item.unit
-        */
-
     }
 
-    func updateDisplay(item: InventoryLocationItem, history: String, total: Double?, display: String) {
-        // NOTE - we pass item in anticipation of item.pack and item.unit
-    
-        // Item.quantity
-        itemValue.text = display
-        if total != nil {
-            itemValue.textColor = UIColor.black
-        } else {
-            itemValue.textColor = UIColor.lightGray
-        }
-        
-        itemHistory.text = history
-        
-        // Item.name
-        guard let item = currentItem.item else {
-            itemName.text = "Error (1)"
-            return
-        }
-        guard let name = item.name else {
-            itemName.text = "Error (2)"
-            return
-        }
-        itemName.text = name
-        
-        // Item.pack
-        
-        // Item.unit
-    }
-    
-    // ALT
+    // func updateDisplay(item: InventoryLocationItem, history: String, total: Double?, display: String) {}
     func updateDisplay(item: InventoryLocationItem, keypadOutput: keypadOutput) {
         // NOTE - we pass item in anticipation of item.pack and item.unit
         
