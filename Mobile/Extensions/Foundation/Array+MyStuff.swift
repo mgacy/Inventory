d3810f111acd6d3acc6b8dc9fba6a5394a7e04bb
mgacy <matt@mgacy.com>
2016-12-04 21:45:35 -0700

    Add Array extension with toDictionary method
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ Mobile/Extensions/Foundation/Array+MyStuff.swift
@@ -0,0 +1,22 @@
+//
+//  Array+MyStuff.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 12/4/16.
+//  Copyright © 2016 Mathew Gacy. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+
+    // http://stackoverflow.com/questions/39791084/swift-3-array-to-dictionary
+    public func toDictionary<Key: Hashable>(with selectKey: (Element) -> Key) -> [Key:Element] {
+        var dict = [Key:Element]()
+        for element in self {
+            dict[selectKey(element)] = element
+        }
+        return dict
+    }
+
+}
