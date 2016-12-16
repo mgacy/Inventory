44ecfb36016d39c44ddd44d0f0be2a2d97f503f8
mgacy <matt@mgacy.com>
2016-12-16 14:10:11 -0700

    Move notification methods into extension
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/OrderItemTVC.swift
+++ Mobile/OrderItemTVC.swift
@@ -141,66 +141,6 @@ class OrderItemTVC: UITableViewController {
         }
     }
 
-    // If we pass a handler, display a "Cancel" and an "OK" button with the latter calling that handler
-    // Otherwise, display a single "OK" button
-    // TODO - shouldn't we allow the specification of the okAction's title?
-    // TODO - should we just present the alert within the function instead of returning it?
-    func createAlert(title: String, message: String, handler: (() -> Void)? = nil) -> UIAlertController {
-
-        // Create alert controller
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-        let cancelTitle: String
-
-        switch handler != nil {
-        case true:
-            cancelTitle = "Cancel"
-
-            // Create and add the OK action
-            let okAction: UIAlertAction = UIAlertAction(title: "OK", style: .default) { action -> Void in
-                // Do some stuff
-                handler!()
-            }
-            alert.addAction(okAction)
-
-        case false:
-            cancelTitle = "OK"
-        }
-
-        // Create and add the Cancel action
-        let cancelAction: UIAlertAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
-        alert.addAction(cancelAction)
-
-        return alert
-    }
-
-    func showAlert(title: String, message: String, handler: (() -> Void)? = nil) {
-
-        // Create alert controller
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-
-        let cancelTitle: String
-        switch handler != nil {
-        case true:
-            cancelTitle = "Cancel"
-
-            // Create and add the OK action
-            let okAction: UIAlertAction = UIAlertAction(title: "OK", style: .default) { action -> Void in
-                // Do some stuff
-                handler!()
-            }
-            alert.addAction(okAction)
-
-        case false:
-            cancelTitle = "OK"
-        }
-
-        // Create and add the Cancel action
-        let cancelAction: UIAlertAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
-        alert.addAction(cancelAction)
-
-        present(alert, animated: true, completion: nil)
-    }
-
     // MARK: - UITableViewDataSource
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -383,6 +323,71 @@ extension OrderItemTVC: NSFetchedResultsControllerDelegate {
 
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.endUpdates()
+    }
+
+}
+
+// MARK: - Notifications
+extension OrderItemTVC {
+
+    // If we pass a handler, display a "Cancel" and an "OK" button with the latter calling that handler
+    // Otherwise, display a single "OK" button
+    // TODO - shouldn't we allow the specification of the okAction's title?
+    // TODO - should we just present the alert within the function instead of returning it?
+    func createAlert(title: String, message: String, handler: (() -> Void)? = nil) -> UIAlertController {
+
+        // Create alert controller
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        let cancelTitle: String
+
+        switch handler != nil {
+        case true:
+            cancelTitle = "Cancel"
+
+            // Create and add the OK action
+            let okAction: UIAlertAction = UIAlertAction(title: "OK", style: .default) { action -> Void in
+                // Do some stuff
+                handler!()
+            }
+            alert.addAction(okAction)
+
+        case false:
+            cancelTitle = "OK"
+        }
+
+        // Create and add the Cancel action
+        let cancelAction: UIAlertAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+
+        return alert
+    }
+
+    func showAlert(title: String, message: String, handler: (() -> Void)? = nil) {
+
+        // Create alert controller
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+
+        let cancelTitle: String
+        switch handler != nil {
+        case true:
+            cancelTitle = "Cancel"
+
+            // Create and add the OK action
+            let okAction: UIAlertAction = UIAlertAction(title: "OK", style: .default) { action -> Void in
+                // Do some stuff
+                handler!()
+            }
+            alert.addAction(okAction)
+
+        case false:
+            cancelTitle = "OK"
+        }
+
+        // Create and add the Cancel action
+        let cancelAction: UIAlertAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
 
 }
