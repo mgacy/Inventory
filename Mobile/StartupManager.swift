39a9134b81eeaaaa6c449d3a706aba863ac719c3
mgacy <matt@mgacy.com>
2016-12-18 14:16:15 -0700

    Add, utilize completion handlers taking …
    advantage of fetchEntityDict method
    
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/StartupManager.swift
+++ Mobile/StartupManager.swift
@@ -29,7 +29,8 @@ class StartupManager {
 
         // Get list of Vendors from server
         print("\nFetching Vendors from server ...")
-        APIManager.sharedInstance.getVendors(storeID: self.storeID, completion: completedGetVendors)
+        //APIManager.sharedInstance.getVendors(storeID: self.storeID, completion: completedGetVendors)
+        APIManager.sharedInstance.getVendors(storeID: self.storeID, completion: syncVendors)
     }
 
     // MARK: - Primary Items
@@ -146,7 +147,83 @@ class StartupManager {
         self.completionHandler(true)
     }
 
-    /*
+    // MARK: - Sync (NEW)
+
+    func syncItems(json: JSON?, error: Error?) {
+        guard error == nil else {
+            print("\(#function) FAILED : \(error)"); return
+        }
+        guard let json = json else {
+            print("\(#function) FAILED : unable to get Items"); return
+        }
+
+        // Create dict from fetch request on Items
+        let prefetch = ["inventoryUnit", "purchaseSubUnit", "purchaseUnit",
+                        "subUnit", "vendor"]
+        guard let itemDict = try? managedObjectContext.fetchEntityDict(Item.self, prefetchingRelationships: prefetch) else {
+            print("\(#function) FAILED : unable to create Item dictionary"); return
+        }
+
+        // Create dict from fetch request on Units
+        guard let unitDict = try? managedObjectContext.fetchEntityDict(Unit.self) else {
+            print("\(#function) FAILED : unable to create Unit dictionary"); return
+        }
+
+        for (_, itemJSON):(String, JSON) in json {
+            guard let itemID = itemJSON["id"].int32 else { break }
+
+            // Find + update / create Items
+            if let existingItem = itemDict[itemID] {
+                //print("UPDATE existing Item: \(existingItem)")
+                existingItem.update(context: managedObjectContext, withJSON: itemJSON)
+                existingItem.updateUnits(withJSON: itemJSON, unitDict: unitDict)
+
+            } else {
+                //print("CREATE new Item: \(itemJSON)")
+                let newItem = Item(context: managedObjectContext, json: itemJSON)
+                newItem.updateUnits(withJSON: itemJSON, unitDict: unitDict)
+            }
+        }
+
+        // TODO - delete Items that were deleted from server
+
+        print("Finished syncing Items")
+        self.completionHandler(true)
+    }
+
+    func syncVendors(json: JSON?, error: Error?) {
+        guard error == nil else {
+            print("\(#function) FAILED : \(error)"); return
+        }
+        guard let json = json else {
+            print("\(#function) FAILED : unable to get Items"); return
+        }
+
+        guard let vendorDict = try? managedObjectContext.fetchEntityDict(Vendor.self) else {
+            print("\(#function) FAILED : unable to create Vendor dictionary"); return
+        }
+
+        for (_, vendorJSON):(String, JSON) in json {
+            guard let itemID = vendorJSON["id"].int32 else { break }
+
+            // Find + update / create Items
+            if let existingEntity = vendorDict[itemID] {
+                //print("UPDATE existing Vendor: \(existingEntity)")
+                existingEntity.update(context: managedObjectContext, withJSON: vendorJSON)
+
+            } else {
+                //print("CREATE new Vendor: \(itemJSON)")
+                _ = Vendor(context: managedObjectContext, json: vendorJSON)
+            }
+        }
+
+        print("Finished with Vendors")
+
+        // Get list of Items from server
+        print("\nFetching Items from server ...")
+        APIManager.sharedInstance.getItems(storeID: self.storeID, completion: syncItems)
+    }
+
     func syncVendors(searchResults: [Vendor], json: JSON) {
 
         // Create dict from array of Vendors returned from fetch request
@@ -177,7 +254,6 @@ class StartupManager {
         print("\nFetching Items from server ...")
         APIManager.sharedInstance.getItems(storeID: self.storeID, completion: completedGetItems)
     }
-    */
 
     // MARK: - Completion
     
