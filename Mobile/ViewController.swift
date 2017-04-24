9409423c668887c48fb4cf8463bed05398ae42d0
mgacy <matt@mgacy.com>
2017-04-24 11:40:05 -0700

    Add unused RootViewController class for tab bar …
    controller
    
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/ViewController.swift
+++ Mobile/ViewController.swift
@@ -41,3 +41,44 @@ protocol RootSectionViewController: class {
 
 }
 
+/*
+// https://blog.krw.io/2016/02/04/dependency-injection-uitabbarcontroller/
+class RootViewController: UITabBarController, UITabBarControllerDelegate {
+
+    // MARK: - Properties
+
+    var userManager: CurrentUserManager!
+    var managedObjectContext: NSManagedObjectContext?
+
+    // MARK: - Lifecycle
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+
+    override func viewDidLoad() {
+        for viewController in self.childViewControllers {
+            guard let vc = viewController as? RootSectionViewController else { fatalError("wrong view controller type") }
+            vc.managedObjectContext = managedObjectContext
+            vc.userManager = userManager
+        }
+        super.viewDidLoad()
+        // NOTE - we must set the delegate to use the method below
+        //self.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    /*
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        // Get your view controller using the correct protocol.
+        // Use guard to make sure the correct type of viewController has been provided.
+        guard let vc = viewController as? SectionNavController
+            else { fatalError("wrong view controller type") }
+        // Assign the protocol variable to whatever you want injected into the class instance.
+        vc.managedObjectContext = _managedObjectContext
+        // This method is declared in the protocol to by type bool, so you need to return a bool.
+        return true
+    }
+    */
+}
+*/
