bbcc4302b6d1411187eee954ac022c216f94dcd4
mgacy <matt@mgacy.com>
2017-04-24 14:10:58 -0700

    Remove unused code
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/InventoryLocationTVC.swift
+++ Mobile/InventoryLocationTVC.swift
@@ -46,13 +46,6 @@ class InventoryLocationTVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations.
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-
         title = "Locations"
         setupTableView()
     }
