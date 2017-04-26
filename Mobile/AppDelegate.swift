77653a06269cd4f1a4978413d5c2a281e06c6270
mgacy <matt@mgacy.com>
2017-04-25 23:53:41 -0700

    Log error on save
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/AppDelegate.swift
+++ Mobile/AppDelegate.swift
@@ -173,6 +173,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                  * The store could not be migrated to the current model version.
                  Check the error message to determine what the actual problem was.
                  */
+                log.error("Unresolved error \(error), \(error.userInfo)")
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
@@ -190,6 +191,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
+                log.error("Unresolved error \(nserror), \(nserror.userInfo)")
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
--- Mobile/InventoryKeypadVC.swift
+++ Mobile/InventoryKeypadVC.swift
@@ -187,6 +187,7 @@ class InventoryKeypadVC: UIViewController {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
+                log.error("Unresolved error \(nserror), \(nserror.userInfo)")
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
