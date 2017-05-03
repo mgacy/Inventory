758826a7a0ce65f839f765e499c6fb6cadfb3697
mgacy <matt@mgacy.com>
2017-05-02 20:11:05 -0700

    Add VendorRep model
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ Mobile/VendorRep+MyStuff.swift
@@ -0,0 +1,49 @@
+//
+//  VendorRep+MyStuff.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 5/2/17.
+//  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import SwiftyJSON
+
+extension VendorRep {
+
+    // MARK: - Lifecycle
+
+    convenience init(context: NSManagedObjectContext, json: JSON) {
+        self.init(context: context)
+        self.update(context: context, withJSON: json)
+    }
+}
+
+extension VendorRep: Syncable {
+
+    public func update(context: NSManagedObjectContext, withJSON json: Any) {
+        guard let json = json as? JSON else {
+            log.error("\(#function) FAILED : SwiftyJSON"); return
+        }
+
+        // Properties
+        /// TODO: remoteID is required
+        if let remoteID = json["rep"]["id"].int32 {
+            self.remoteID = remoteID
+        }
+        if let firstName = json["rep"]["first_name"].string {
+            self.firstName = firstName
+        }
+        if let lastName = json["rep"]["last_name"].string {
+            self.lastName = lastName
+        }
+        if let email = json["rep"]["email"].string {
+            self.email = email
+        }
+        if let phone = json["rep"]["phone"].string {
+            self.phone = phone
+        }
+    }
+
+}
