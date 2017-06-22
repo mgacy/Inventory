fb4c657ad19ea2092fdcd4a7d9a88873b48eda6e
mgacy <matt@mgacy.com>
2017-06-22 15:02:48 -0700

    Move Dynamic into separate file
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/OrderViewModel.swift
+++ Mobile/OrderViewModel.swift
@@ -10,38 +10,6 @@ import Foundation
 //import CoreData
 import SwiftyJSON
 
-//
-//  Dynamic.swift
-//  MVVMExample
-//
-//  Created by Dino Bartosak on 25/09/16.
-//  Copyright © 2016 Toptal. All rights reserved.
-//
-
-class Dynamic<T> {
-    typealias Listener = (T) -> ()
-    var listener: Listener?
-
-    func bind(_ listener: Listener?) {
-        self.listener = listener
-    }
-
-    func bindAndFire(_ listener: Listener?) {
-        self.listener = listener
-        listener?(value)
-    }
-
-    var value: T {
-        didSet {
-            listener?(value)
-        }
-    }
-
-    init(_ val: T) {
-        value = val
-    }
-}
-
 class OrderViewModel {
 
     typealias CompletionHandlerType = (JSON?, Error?) -> Void
@@ -128,6 +96,8 @@ class OrderViewModel {
     }
 
 }
+
+// MARK: - Various Classes, Extensions
 
 // Mobile Dan
 // https://stackoverflow.com/a/41668104
