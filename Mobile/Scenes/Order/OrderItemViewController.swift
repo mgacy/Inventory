825bcc20180047eefa5f93efa6bf2f0aa24043bb
mgacy <matt@mgacy.com>
2017-06-22 23:45:37 -0700

    Add outlets, actions for buttons and completion …
    handlers
    
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/Scenes/Order/OrderItemViewController.swift
+++ Mobile/Scenes/Order/OrderItemViewController.swift
@@ -38,6 +38,9 @@ class OrderItemViewController: UIViewController {
 
     // MARK: - Display Outlets
     @IBOutlet weak var repNameTextLabel: UILabel!
+    @IBOutlet weak var messageButton: RoundButton!
+    @IBOutlet weak var emailButton: RoundButton!
+    @IBOutlet weak var callButton: RoundButton!
     @IBOutlet weak var tableView: UITableView!
 
     // MARK: - Lifecycle
@@ -72,7 +75,7 @@ class OrderItemViewController: UIViewController {
         guard
             let indexPath = self.tableView.indexPathForSelectedRow?.row,
             let managedObjectContext = managedObjectContext else {
-                fatalError("Unable to get indexPath or moc")
+                fatalError("\(#function) FAILED: unable to get indexPath or moc")
         }
 
         destinationController.viewModel = OrderKeypadViewModel(for: parentObject, atIndex: indexPath,
@@ -104,19 +107,81 @@ class OrderItemViewController: UIViewController {
                                          fetchedResultsController: frc, delegate: self)
     }
 
-    // MARK: - User Actions
-
     func setupView() {
         repNameTextLabel.text = viewModel.repName
 
         /// NOTE: disable for testing
         guard messageComposer.canSendText() else {
-            //messageButton.isEnabled = false
+            messageButton.isEnabled = false
             return
         }
 
         /// TODO: handle orders that have been placed but not uploaded; display different `upload` button
-        //messageButton.isEnabled = viewModel.canMessageOrder
+        messageButton.isEnabled = viewModel.canMessageOrder
+    }
+
+    // MARK: - User Actions
+
+    @IBAction func tappedMessageButton(_ sender: Any) {
+        log.debug("Send message ...")
+
+        // Simply POST the order if we already sent the message but were unable to POST it previously
+
+        guard let phoneNumber = parentObject.vendor?.rep?.phone else {
+            log.error("Unable to get phoneNumber"); return
+        }
+        //let phoneNumber = "602-980-4718"
+        guard let message = viewModel.orderMessage else {
+            log.error("\(#function) FAILED : unable to getOrderMessage"); return
+        }
+
+        let messageComposeVC = messageComposer.configuredMessageComposeViewController(
+            phoneNumber: phoneNumber, message: message,
+            completionHandler: completedPlaceOrder)
+        present(messageComposeVC, animated: true, completion: nil)
+    }
+
+    @IBAction func tappedEmailButton(_ sender: Any) {
+        log.debug("Email message ...")
+    }
+
+}
+
+// MARK: - Completion Handlers
+extension OrderItemViewController {
+
+    func completedPlaceOrder(_ result: MessageComposeResult) {
+        switch result {
+        case .cancelled:
+            log.info("Message was cancelled")
+        case .failed:
+            log.error("\(#function) FAILED : unable to send Order message")
+            showAlert(title: "Problem", message: "Unable to send Order message")
+        case .sent:
+            log.info("Sent Order message")
+            HUD.show(.progress)
+            /// TODO: simply pass closure?
+            viewModel.postOrder(completion: completedPostOrder)
+        }
+    }
+
+    /// TODO: change completion handler to accept standard (JSON?, Error?)
+
+    func completedPostOrder(succeeded: Bool, json: JSON) {
+        if succeeded {
+            viewModel.completedPostOrder()
+
+            // swiftlint:disable:next unused_closure_parameter
+            HUD.flash(.success, delay: 0.5) { finished in
+                // Pop view
+                self.navigationController!.popViewController(animated: true)
+            }
+
+        } else {
+            log.error("\(#function) FAILED : unable to POST order \(json)")
+            HUD.flash(.error, delay: 1.0)
+            //showAlert(title: "Problem", message: "Unable to upload Order")
+        }
     }
 
 }
