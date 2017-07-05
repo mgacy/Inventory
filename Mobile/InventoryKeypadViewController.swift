bf61ed0d9b0608e1f9c38cd559a96c92752d4802
mgacy <matt@mgacy.com>
2017-07-04 19:29:43 -0700

    Use managedObjectContext.performSaveOrRollback
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/InventoryKeypadViewController.swift
+++ Mobile/InventoryKeypadViewController.swift
@@ -158,16 +158,7 @@ class InventoryKeypadViewController: UIViewController {
             }
 
             // Save the context.
-            let context = self.managedObjectContext!
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                log.error("Unresolved error \(nserror), \(nserror.userInfo)")
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+            managedObjectContext?.performSaveOrRollback()
         }
 
         updateDisplay(item: currentItem, keypadOutput: output)
