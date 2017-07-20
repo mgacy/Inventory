d3cc51260a4e723a32760c7621b6cbe2b5604399
mgacy <matt@mgacy.com>
2017-07-20 14:46:13 -0700

    Add new model version with manually managed …
    classes for Inventory, Order, Invoice
    
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ Mobile/Models/Inventory+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Inventory+CoreDataClass.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 7/20/17.
+//  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Inventory)
+public class Inventory: NSManagedObject {
+
+}
---
+++ Mobile/Models/Invoice+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Invoice+CoreDataClass.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 7/20/17.
+//  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Invoice)
+public class Invoice: NSManagedObject {
+
+}
