e72a3ca48b5100bc53b00963f311d732d7411ebb
mgacy <matt@mgacy.com>
2017-07-20 19:00:19 -0700

    Add cached date formatter as extension to …
    Formatter and Date; add extension to String
    
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/Extensions/Foundation/NSDate+MyStuff.swift
+++ Mobile/Extensions/Foundation/NSDate+MyStuff.swift
@@ -8,6 +8,26 @@
 
 import Foundation
 
+/*
+ * If you cache date formatters (or any other objects that depend on the user’s current locale), you should subscribe 
+ * to the `NSCurrentLocaleDidChangeNotification` notification and update your cached objects when the current locale
+ * changes. [Source](https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/DataFormatting/Articles/dfDateFormatting10_4.html#//apple_ref/doc/uid/TP40002369-SW10)
+ */
+
+extension Formatter {
+
+    // Inspired by: https://stackoverflow.com/a/43658213/4472195
+    // See also:    https://stackoverflow.com/a/42370648/4472195
+    static let basicDate: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        //formatter.timeZone = TimeZone(abbreviation: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+}
+
 // https://stackoverflow.com/questions/28332946/nsdateformatter-stringfromdatensdate-returns-empty-string
 extension NSDate {
 
@@ -31,6 +51,26 @@ extension NSDate {
 
 }
 
-// Alternatively, see:
-// https://stackoverflow.com/questions/36805662/use-swiftyjson-to-deserialize-nsdate
-// https://grokswift.com/nsdate-webservices/
+extension String {
+
+    func toBasicDate() -> Date? {
+        return Formatter.basicDate.date(from: self)
+    }
+
+}
+
+extension Date {
+
+    static let basicDate: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        //formatter.timeZone = TimeZone(abbreviation: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+
+    func stringFromDate() -> String? {
+        return Date.basicDate.string(from: self)
+    }
+
+}
