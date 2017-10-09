b5f7642bb07021a193d9246d5b148dc2aee12921
mgacy <matt@mgacy.com>
2017-10-09 12:35:07 -0700

    Add DataManager
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ Mobile/DataManager.swift
@@ -0,0 +1,480 @@
+//
+//  DataManager.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 10/2/17.
+//  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import Alamofire
+//import RxCocoa
+import RxSwift
+
+class DataManager {
+
+    // MARK: - Properties
+
+    /// TODO: specify client as conforming to NetworkServiceType protocol
+    private let client: APIManager
+    let managedObjectContext: NSManagedObjectContext
+    //let viewContext: NSManagedObjectContext
+    //let syncContext: NSManagedObjectContext
+    /// TODO: use UserManagerType; should userManager be private?
+    let userManager: CurrentUserManager
+
+    // MARK: - Lifecycle
+
+    init(context: NSManagedObjectContext, userManager: CurrentUserManager) {
+        self.managedObjectContext = context
+        self.userManager = userManager
+        self.client = APIManager.sharedInstance
+    }
+
+    // MARK: General
+
+    func refreshStuff() -> Observable<Bool> {
+        return refreshVendors()
+            // Items
+            .flatMap { result -> Observable<Bool> in
+                //log.debug("\(#function) - \(result)")
+                return self.refreshItems()
+            }
+            .flatMap { result -> Observable<Bool> in
+                //log.debug("\(#function) - \(result)")
+                return Observable.just(self.managedObjectContext.saveOrRollback())
+        }
+        //.materialize()
+    }
+
+    // Item
+
+    //func createItem() -> Observable<> {}
+
+    //func deleteItem(_ item: Item) -> Observable<> {}
+
+    //func updateItem(_ item: Item) -> Observable<Bool> { return Observable.just(true) }
+
+    //func refreshItems() -> Observable<Event<[Items]>> {
+    func refreshItems() -> Observable<Bool> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(false)
+            //.materialize()
+        }
+
+        return client.getItems(storeID: storeID)
+            .map { [weak self] response in
+                switch response.result {
+                case .success(let records):
+                    guard let context = self?.managedObjectContext else { return false }
+                    Item.sync(with: records, in: context)
+                    /*
+                    do {
+                        try self?.managedObjectContext.syncEntitiesNew(Item.self, with: records)
+                    } catch let error {
+                        log.error("\(#function) FAILED : \(error)")
+                        return false
+                    }
+                     */
+                    return true
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    return false
+                }
+        }
+        //.materialize()
+    }
+
+    // ItemCategory
+    /*
+    func refreshItemCategories() -> Observable<Bool> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(false)
+        }
+
+        return client.getItemCategories(storeID: storeID)
+            .map { [weak self] response in
+                switch response.result {
+                case .success(let records):
+                    do {
+                        try self?.managedObjectContext.syncEntities(ItemCategory.self, with: records)
+                    } catch let error {
+                        log.error("\(#function) FAILED : \(error)")
+                        return false
+                    }
+                    return true
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    return false
+                }
+        }
+    }
+     */
+    // Vendor
+
+    //func createVendor() -> Observable<Vendor> {}
+
+    func refreshVendors() -> Observable<Bool> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(false)
+        }
+        return client.getVendors(storeID: storeID)
+            .map { [weak self] response in
+                switch response.result {
+                case .success(let records):
+                    guard let context = self?.managedObjectContext else { return false }
+                    Vendor.sync(with: records, in: context)
+                    /*
+                    do {
+                        try self?.managedObjectContext.syncEntities(Vendor.self, with: records)
+                    } catch let error {
+                        log.error("\(#function) FAILED : \(error)")
+                        return false
+                    }
+                     */
+                    return true
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    return false
+                }
+        }
+    }
+
+}
+
+// MARK: - Inventory
+extension DataManager {
+
+    //func createInventory() -> Observable<> {}
+
+    //func deleteInventory(_ inventory: Inventory) -> Observable<> {}
+
+    //func updateInventory(_ inventory: Inventory) -> Observable<Bool> { return Observable.just(true) }
+
+    //func refreshInventories() -> Observable<Event<[Inventory]>> {
+    func refreshInventories() -> Observable<Bool> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(false)
+            //.materialize()
+        }
+
+        return client.getInventories(storeID: storeID)
+            .map { [weak self] response in
+                switch response.result {
+                case .success(let records):
+                    guard let context = self?.managedObjectContext else { return false }
+                    Inventory.sync(with: records, in: context)
+                    /*
+                    do {
+                        try self?.managedObjectContext.syncEntitiesNew(Item.self, with: records)
+                    } catch let error {
+                        log.error("\(#function) FAILED : \(error)")
+                        return false
+                    }
+                     */
+                    return true
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    return false
+                }
+        }
+        //.materialize()
+    }
+}
+
+// MARK: - Order
+
+extension DataManager {
+
+    func createOrderCollection(generationMethod method: NewOrderGenerationMethod, returnUsage: Bool, periodLength: Int?) -> Observable<Event<OrderCollection>> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.error(BackendError.myError(error: "Missing storeID"))
+            .materialize()
+        }
+
+        //let storeID = 5
+        return client.postOrderCollection(storeID: storeID, generationMethod: method, returnUsage: returnUsage,
+                                          periodLength: periodLength)
+            .flatMap { [weak self] response -> Observable<OrderCollection> in
+                switch response.result {
+                case .success(let record):
+                    guard let context = self?.managedObjectContext else {
+                        throw BackendError.myError(error: "Missing MOC")
+                    }
+                    guard let date = record.date.toBasicDate() else {
+                        log.error("\(#function) FAILED : unable to parse date")
+                        throw BackendError.myError(error: "Unable to parse date")
+                    }
+
+                    let newCollection: OrderCollection = context.insertObject()
+                    newCollection.dateTimeInterval = date.timeIntervalSinceReferenceDate
+                    newCollection.update(with: record, in: context)
+                    return Observable.just(newCollection)
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    throw error
+                }
+            }
+            .materialize()
+    }
+
+    func refreshOrderCollection(_ collection: OrderCollection) -> Observable<OrderCollection> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(collection)
+            //.materialize()
+        }
+        guard let dateString = collection.date.stringFromDate() else {
+            log.error("\(#function) FAILED: unable to get dateString")
+            return Observable.just(collection)
+            //.materialize()
+        }
+        return client.getOrderCollection(storeID: storeID, orderDate: dateString)
+            .map { [weak self] response in
+                switch response.result {
+                case .success(let record):
+                    guard let context = self?.managedObjectContext else { return collection }
+                    collection.update(with: record, in: context)
+                    return collection
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    return collection
+                }
+                //return collection
+        }
+    }
+
+    func refreshOrderCollections() -> Observable<Bool> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(false)
+            //return Observable.error(BackendError.myError(error: "Missing storeID"))
+            //.materialize()
+        }
+
+        return client.getOrderCollections(storeID: storeID)
+            .map { [weak self] response in
+                switch response.result {
+                case .success(let records):
+                    guard let context = self?.managedObjectContext else { return false }
+                    OrderCollection.sync(with: records, in: context)
+                    return true
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    //throw error
+                    return false
+                }
+        }
+        //.materialize()
+    }
+
+}
+
+// MARK: - Invoice
+
+extension DataManager {
+
+    func refreshInvoiceCollection(_ collection: InvoiceCollection) -> Observable<InvoiceCollection> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(collection)
+            //.materialize()
+        }
+
+        //let date = collection.date
+        guard let dateString = collection.date.stringFromDate() else {
+            log.error("\(#function) FAILED: unable to get dateString")
+            return Observable.just(collection)
+            //.materialize()
+        }
+        return client.getInvoiceCollection(storeID: storeID, invoiceDate: dateString)
+            .map { [weak self] response in
+                switch response.result {
+                case .success(let record):
+                    guard let context = self?.managedObjectContext else { return collection }
+                    collection.update(with: record, in: context)
+                    return collection
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    return collection
+                }
+                //return collection
+            }
+    }
+
+    func refreshInvoiceCollections() -> Observable<Bool> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(false)
+            //.materialize()
+        }
+
+        return client.getInvoiceCollections(storeID: storeID)
+            .map { [weak self] response in
+                switch response.result {
+                case .success(let records):
+                    guard let context = self?.managedObjectContext else { return false }
+                    InvoiceCollection.sync(with: records, in: context)
+                    return true
+                case .failure(let error):
+                    log.warning("\(#function) FAILED : \(error)")
+                    return false
+                }
+        }
+        //.materialize()
+    }
+
+}
+
+// MARK: - Attempt at Generic Method
+/*
+extension DataManager {
+
+    func test(records: [RemoteMenuItem]) -> Observable<Void> {
+        try? managedObjectContext.syncEntitiesNew(MenuItem.self, with: records)
+        return Observable.just(())
+    }
+
+    func refreshItemsAlt() -> Observable<Bool> {
+        guard let storeID = userManager.storeID else {
+            log.error("\(#function) FAILED: no storeID")
+            return Observable.just(false)
+        }
+        return client.getItems(storeID: storeID)
+            .flatMap { response -> Bool in
+                return self.syncEntitiesB(Item.self, with: response)
+                //return true
+        }
+    }
+
+    private func syncEntitiesA<M: NewSyncable, R>(_ entity: M, with response: DataResponse<[R]>) -> Bool where R == M.RemoteType {
+        //return true
+        switch response.result {
+        case .success(let records):
+            do {
+                try managedObjectContext.syncEntitiesNew(M, with: records)
+            } catch let error {
+                log.error("\(#function) FAILED : \(error)")
+                return false
+            }
+            return true
+        case .failure(let error):
+            log.warning("\(#function) FAILED : \(error)")
+            return false
+        }
+    }
+
+    private func syncEntitiesB<M: NewSyncable, R>(_ entity: M, with response: DataResponse<[R]>) -> Bool where M: NSManagedObject, R == M.RemoteType {
+        switch response.result {
+        case .success(let records):
+            M.sync(with: records, in: managedObjectContext)
+            /*
+             do {
+             try managedObjectContext.syncEntitiesNew(M, with: records)
+             } catch let error {
+             log.error("\(#function) FAILED : \(error)")
+             return false
+             }
+             */
+            return true
+        case .failure(let error):
+            log.warning("\(#function) FAILED : \(error)")
+            return false
+        }
+    }
+
+}
+*/
+// MARK: - Authentication
+
+/// TODO: make this conform to a protocol?
+extension DataManager {
+
+    /// TODO: mark as @discardable and return Observable<User>?
+    public func login(email: String, password: String) -> Observable<Bool> {
+        return Observable.create { observer in
+            self.userManager.login(email: email, password: password) { error in
+                if let error = error {
+                    log.warning("\(#function) ERROR : \(error)")
+                    //observer.onError(error)
+                    observer.onNext(true)
+                    observer.onCompleted()
+                } else {
+                    log.debug("We logged in")
+                    observer.onNext(true)
+                    observer.onCompleted()
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    public func logout() -> Observable<Bool> {
+        /// TODO: return if already logged out
+        //return Observable.create { observer in
+        self.userManager.logout { success in
+            log.debug("logout result: \(success)")
+            switch success {
+            case true:
+                log.verbose("Logout: Success")
+            case false:
+                log.verbose("Logout: Failure")
+            }
+            //let deletionResult = self.deleteData(in: self.managedObjectContext)
+        }
+        // NOTE: this currently starts deleting data before we have received a response from server
+        log.verbose("Deleting data ...")
+        return deleteData(in: self.managedObjectContext)
+
+        //return Disposables.create()
+        //}
+    }
+
+    public func signUp(username: String, email: String, password: String) -> Observable<Bool> {
+        var success: Bool = false
+        self.userManager.signUp(username: username, email: email, password: password) { error in
+            log.debug("\(#function) : \(String(describing: error))")
+            success = true
+        }
+        return Observable.just(success)
+    }
+
+    private func deleteData(in context: NSManagedObjectContext) -> Observable<Bool> {
+        /// TODO: use cascade rules to reduce list of entities we need to manually delete
+
+        // Inventory
+
+        // Order
+
+        // Invoice
+
+        // Item
+        do {
+            try managedObjectContext.deleteEntities(Item.self)
+        } catch {
+            log.error("\(#function) FAILED: unable to delete Items")
+        }
+        // ItemCategory
+        do {
+            try managedObjectContext.deleteEntities(ItemCategory.self)
+        } catch {
+            log.error("\(#function) FAILED: unable to delete ItemCategories")
+        }
+        // Vendor
+        do {
+            try managedObjectContext.deleteEntities(Vendor.self)
+        } catch {
+            log.error("\(#function) FAILED: unable to delete Vendors")
+        }
+
+        let result = managedObjectContext.saveOrRollback()
+        log.info("Save result: \(result)")
+        return Observable.just(result)
+    }
+}
