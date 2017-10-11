f34171d09b27875b789449fba4e8c3a801bbf507
mgacy <matt@mgacy.com>
2017-10-10 18:13:59 -0700

    Fix optionality of RemoteInventory and nested …
    Item properties; reorganize
    
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/Models/RemoteInventory.swift
+++ Mobile/Models/RemoteInventory.swift
@@ -10,6 +10,8 @@ import Foundation
 
 // swiftlint:disable nesting
 
+// MARK: - Inventory (New)
+
 struct RemoteInventory: Codable {
 
     struct Category: Codable {
@@ -30,6 +32,7 @@ struct RemoteInventory: Codable {
         struct Par: Codable {
             let par: Double
             let unitID: Int
+
             private enum CodingKeys: String, CodingKey {
                 case par
                 case unitID = "unit_id"
@@ -38,36 +41,39 @@ struct RemoteInventory: Codable {
 
         let remoteID: Int
         let name: String
-        let categoryID: Int
-        let packSize: Int
-        let par: Par
-        let subSize: Double
-        let subUnitID: Int
-        let inventoryUnitID: Int
-        let purchaseUnitID: Int
-        let purchaseSubUnitID: Int
+        let packSize: Int?
+        let subSize: Double?
+        // Foreign Keys
+        let categoryID: Int?
+        let inventoryUnitID: Int?
+        let purchaseSubUnitID: Int?
+        let purchaseUnitID: Int?
+        let subUnitID: Int?
+        // Relationships
+        let par: Par?
 
         private enum CodingKeys: String, CodingKey {
             case remoteID = "id"
             case name
-            case categoryID = "category_id"
             case packSize = "pack_size"
-            case par
             case subSize = "sub_size"
-            case subUnitID = "sub_unit_id"
+            case categoryID = "category_id"
             case inventoryUnitID = "inventory_unit_id"
-            case purchaseUnitID = "purchase_unit_id"
             case purchaseSubUnitID = "purchase_sub_unit_id"
+            case purchaseUnitID = "purchase_unit_id"
+            case subUnitID = "sub_unit_id"
+            case par
         }
     }
 
-    let remoteID: Int
+    let remoteID: Int?
     //let date: Date
     let date: String
     let inventoryTypeID: Int?
     let storeID: Int
-    let items: [Item]?
+    // Relationships
     let categories: [Category]?
+    let items: [Item]?
     let locations: [RemoteInventoryLocation]?
 
     private enum CodingKeys: String, CodingKey {
@@ -75,15 +81,15 @@ struct RemoteInventory: Codable {
         case date
         case inventoryTypeID = "inventory_type_id"
         case storeID = "store_id"
-        case items
         case categories
+        case items
         case locations
     }
 }
 
 extension RemoteInventory: RemoteRecord {
     typealias SyncIdentifierType = Int32
-    var syncIdentifier: SyncIdentifierType { return Int32(self.remoteID) }
+    var syncIdentifier: SyncIdentifierType { return Int32(self.remoteID ?? 0) }
 }
 
 // MARK: - InventoryLocation
