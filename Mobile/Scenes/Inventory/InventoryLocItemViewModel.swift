2e97492dcecb3fc4fbbd5f0f28c79b6118690ec5
mgacy <matt@mgacy.com>
2017-10-23 22:59:03 -0700

    Add InventoryLocItemViewModel
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ Mobile/Scenes/Inventory/InventoryLocItemViewModel.swift
@@ -0,0 +1,74 @@
+//
+//  InventoryLocItemViewModel.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 10/23/17.
+//  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+
+import CoreData
+//import RxCocoa
+//import RxSwift
+
+struct InventoryLocItemViewModel {
+
+    // MARK: - Properties
+
+    let dataManager: DataManager
+    var parentObject: LocationItemListParent
+
+    // CoreData
+    //private let filter: NSPredicate? = nil
+    private let sortDescriptors = [NSSortDescriptor(key: "position", ascending: true),
+                                   NSSortDescriptor(key: "item.name", ascending: true)]
+    private let cacheName: String? = nil
+    private let sectionNameKeyPath: String? = nil
+    private let fetchBatchSize = 20 // 0 = No Limit
+
+    // MARK: - Input
+
+    // MARK: - Output
+    let frc: NSFetchedResultsController<InventoryLocationItem>
+    let windowTitle: String
+
+    // MARK: - Lifecycle
+
+    init(dataManager: DataManager, parentObject: LocationItemListParent) {
+        self.dataManager = dataManager
+        self.parentObject = parentObject
+
+        // Title
+        switch self.parentObject {
+        case .category(let parentCategory):
+            self.windowTitle = parentCategory.name ?? "Error"
+        case .location(let parentLocation):
+            self.windowTitle = parentLocation.name ?? "Error"
+        //case .none:
+        //    self.windowTitle = "Error (2)"
+        }
+
+        // FetchRequest
+        let request: NSFetchRequest<InventoryLocationItem> = InventoryLocationItem.fetchRequest()
+        request.predicate = parentObject.fetchPredicate
+        /*
+        switch self.parentObject {
+        case .category(let parentCategory):
+            request.predicate = NSPredicate(format: "category == %@", parentCategory)
+        //self.windowTitle = parentCategory.name ?? "Error"
+        case .location(let parentLocation):
+            request.predicate = NSPredicate(format: "location == %@", parentLocation)
+        //self.windowTitle = parentLocation.name ?? "Error"
+        case .none:
+            fatalError("\(#function) FAILED : parentObject not set")
+        }
+        */
+        request.sortDescriptors = sortDescriptors
+        request.fetchBatchSize = fetchBatchSize
+        request.returnsObjectsAsFaults = false
+
+        let managedObjectContext = dataManager.managedObjectContext
+        self.frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: managedObjectContext,
+                                              sectionNameKeyPath: sectionNameKeyPath, cacheName: cacheName)
+    }
+
+}
