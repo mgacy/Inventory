d7e37ca04800c2e3e1b8504dc32ab8ef50cb2bdb
mgacy <matt@mgacy.com>
2017-11-27 18:24:27 -0700

    Add alternate LoginCoordinator supporting root …
    or modal presentation
    
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/Scenes/Initial/InitialLoginCoordinator.swift
+++ Mobile/Scenes/Initial/InitialLoginCoordinator.swift
@@ -47,3 +47,86 @@ class InitialLoginCoordinator: BaseCoordinator<Void> {
     }
 
 }
+
+// MARK: - Alt
+/*
+enum CoordinatorMode {
+    case root(UIWindow)
+    case section(UINavigationController)
+    case modal(UIViewController)
+}
+*/
+class LoginCoordinator: BaseCoordinator<Void> {
+
+    private let window: UIWindow?
+    private let rootViewController: UIViewController?
+    private let dataManager: DataManager
+
+    init(window: UIWindow, dataManager: DataManager) {
+        self.window = window
+        self.rootViewController = nil
+        self.dataManager = dataManager
+    }
+
+    init(rootViewController: UIViewController, dataManager: DataManager) {
+        self.window = nil
+        self.rootViewController = rootViewController
+        self.dataManager = dataManager
+    }
+
+    override func start() -> Observable<CoordinationResult> {
+        let viewController = InitialLoginViewController.initFromStoryboard(name: "Main")
+        let viewModel = InitialLoginViewModel(dataManager: dataManager)
+        viewController.viewModel = viewModel
+
+        if let `window` = window {
+            // Display as initial window
+            log.debug("\(#function) : display as root view controller of: \(window)")
+            window.rootViewController = viewController
+            window.makeKeyAndVisible()
+
+            let signedUp = viewController.signupButton.rx.tap
+                .flatMap { _ -> Observable<SignupCoordinationResult> in
+                    return self.showSignup(on: viewController)
+                }
+                .filter { $0 != SignupCoordinationResult.cancel }
+                .map { _ in return }
+
+            return Observable.merge(viewController.didLogin, signedUp)
+                .take(1)
+
+        } else {
+            // Display in NavigationController
+            log.debug("\(#function) : display in navigation controller")
+            guard let rootViewController = rootViewController else {
+                fatalError("\(#function) FAILED : unable to get expected root view controller")
+            }
+
+            let cancelled = viewController.cancelButtonItem.rx.tap.asObservable()
+            /*
+            let signedUp = viewController.signupButton.rx.tap
+                .flatMap { _ -> Observable<SignupCoordinationResult> in
+                    return self.showSignup(on: viewController)
+                }
+                .filter { $0 != SignupCoordinationResult.cancel }
+                .map { _ in return }
+            */
+
+            let navigationController = UINavigationController(rootViewController: viewController)
+            rootViewController.present(navigationController, animated: true)
+
+            return Observable.merge(viewController.didLogin, cancelled)
+                .take(1)
+                .do(onNext: { _ in rootViewController.dismiss(animated: true) })
+        }
+    }
+
+    // MARK: - Sections
+
+    private func showSignup(on rootViewController: UIViewController) -> Observable<SignupCoordinationResult> {
+        let signupCoordinator = SignupCoordinator(rootViewController: rootViewController, dataManager: self.dataManager)
+        return coordinate(to: signupCoordinator)
+        //.filter { $0 != .cancel }
+    }
+
+}
