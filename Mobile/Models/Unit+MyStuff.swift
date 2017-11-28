59293e34485636c6663db85a60fe6ada097c48be
mgacy <matt@mgacy.com>
2017-11-28 14:26:25 -0700

    Remove Syncable conformance
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/Models/Unit+MyStuff.swift
+++ Mobile/Models/Unit+MyStuff.swift
@@ -6,36 +6,7 @@
 //  Copyright © 2016 Mathew Gacy. All rights reserved.
 //
 
-import Foundation
 import CoreData
-import SwiftyJSON
-
-extension Unit: Syncable {
-
-    // MARK: - Lifecycle
-
-    convenience init(context: NSManagedObjectContext, json: JSON) {
-        self.init(context: context)
-        self.update(context: context, withJSON: json)
-    }
-
-    public func update(context: NSManagedObjectContext, withJSON json: JSON) {
-        // guard let json = json as? JSON else {
-        //     log.error("\(#function) FAILED : SwiftyJSON"); return
-        // }
-
-        if let remoteID = json["id"].int32 {
-            self.remoteID = remoteID
-        }
-        if let name = json["name"].string {
-            self.name = name
-        }
-        if let abbreviation = json["abbreviation"].string {
-            self.abbreviation = abbreviation
-        }
-    }
-
-}
 
 extension Unit: NewSyncable {
     typealias RemoteType = RemoteUnit
