bb6c38f382008c6f4e8b4d1e4a9b24d41a481221
mgacy <matt@mgacy.com>
2017-11-28 14:16:22 -0700

    Remove SwiftyJSON dependency
    Signed-off-by: mgacy <matt@mgacy.com>

--- Mobile/Core Data Helpers/CoreDataImporter.swift
+++ Mobile/Core Data Helpers/CoreDataImporter.swift
@@ -11,7 +11,7 @@
 //
 
 import CoreData
-import SwiftyJSON
+import UIKit
 
 class CoreDataImporter {
 
@@ -39,14 +39,13 @@ class CoreDataImporter {
             log.error("Invalid filename.")
             return false
         }
-        let jsonArray = JSON(data: asset.data)
-        guard jsonArray != JSON.null else {
-            log.error("Could not get json from file, make sure that file contains valid json.")
-            return false
-        }
 
-        for (_, unitJSON):(String, JSON) in jsonArray {
-            _ = Unit(context: context, json: unitJSON)
+        let decoder = JSONDecoder()
+        do {
+            let units = try decoder.decode([RemoteUnit].self, from: asset.data)
+            Unit.sync(with: units, in: context)
+        } catch {
+            log.error("\(#function) FAILED : error trying to decode Unit data")
         }
 
         if context.saveOrRollback() == true {
