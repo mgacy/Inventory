3b6cfe6b4754038dbe421eeb823336fe5d66851a
mgacy <matt@mgacy.com>
2017-12-12 23:06:52 -0700

    Add EmptyDetailViewController
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ Mobile/Scenes/Common/EmptyDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EmptyDetailViewController.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 12/12/17.
+//  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+
+import UIKit
+
+class EmptyDetailViewController: UIViewController {
+
+    /// TODO: is there any need to do this?
+    lazy var mainView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // meh ...
+        setupView()
+        setupConstraints()
+    }
+
+    func setupView() {
+        self.view.addSubview(mainView)
+    }
+
+    func setupConstraints() {
+        mainView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        mainView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        mainView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        mainView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+
+}
