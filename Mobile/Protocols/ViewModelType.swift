f7fa8bb2b26b29c6fd0adc77708ab521f0a6eafd
mgacy <matt@mgacy.com>
2018-02-06 21:24:46 -0700

    Minor changes

--- Mobile/Protocols/ViewModelType.swift
+++ Mobile/Protocols/ViewModelType.swift
@@ -2,11 +2,11 @@
 //  ViewModelType.swift
 //  Mobile
 //
-// Martin Moizard
-// https://medium.com/blablacar-tech/rxswift-mvvm-66827b8b3f10
-//
 //  Created by Mathew Gacy on 11/14/17.
 //  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+//  Martin Moizard
+//  https://medium.com/blablacar-tech/rxswift-mvvm-66827b8b3f10
 //
 
 import Foundation
