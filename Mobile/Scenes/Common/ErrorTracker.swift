c7b9089f91830ad81ad8c62e5c3c212e486d3e1d
mgacy <matt@mgacy.com>
2018-02-08 23:09:38 -0700

    Add ErrorTracker

---
+++ Mobile/Scenes/Common/ErrorTracker.swift
@@ -0,0 +1,45 @@
+//
+//  ErrorTracker.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 2/8/18.
+//  Copyright © 2018 Mathew Gacy. All rights reserved.
+//
+//  From:
+//  https://github.com/sergdort/CleanArchitectureRxSwift
+//  Copyright © 2017 Sergey Shulga. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+final class ErrorTracker: SharedSequenceConvertibleType {
+    typealias SharingStrategy = DriverSharingStrategy
+    private let _subject = PublishSubject<Error>()
+
+    func trackError<O: ObservableConvertibleType>(from source: O) -> Observable<O.E> {
+        return source.asObservable().do(onError: onError)
+    }
+
+    func asSharedSequence() -> SharedSequence<SharingStrategy, Error> {
+        return _subject.asObservable().asDriverOnErrorJustComplete()
+    }
+
+    func asObservable() -> Observable<Error> {
+        return _subject.asObservable()
+    }
+
+    private func onError(_ error: Error) {
+        _subject.onNext(error)
+    }
+
+    deinit {
+        _subject.onCompleted()
+    }
+}
+
+extension ObservableConvertibleType {
+    func trackError(_ errorTracker: ErrorTracker) -> Observable<E> {
+        return errorTracker.trackError(from: self)
+    }
+}
