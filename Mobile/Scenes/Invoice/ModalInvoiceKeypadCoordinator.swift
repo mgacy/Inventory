724d9b6820499d54e68b54a9ffb64380e0ce4e72
mgacy <matt@mgacy.com>
2018-05-22 22:19:59 -0700

    Split into separate file

---
+++ Mobile/Scenes/Invoice/ModalInvoiceKeypadCoordinator.swift
@@ -0,0 +1,50 @@
+//
+//  ModalInvoiceKeypadCoordinator.swift
+//  Mobile
+//
+//  Created by Mathew Gacy on 5/22/18.
+//  Copyright © 2018 Mathew Gacy. All rights reserved.
+//
+
+import RxCocoa
+import RxSwift
+
+final class ModalInvoiceKeypadCoordinator: BaseCoordinator<Void> {
+    typealias Dependencies = HasDataManager
+
+    private let rootViewController: UIViewController
+    private let dependencies: Dependencies
+    private let invoice: Invoice
+    private let index: Int
+
+    init(rootViewController: UIViewController, dependencies: Dependencies, invoice: Invoice, atIndex index: Int) {
+        self.rootViewController = rootViewController
+        self.dependencies = dependencies
+        self.invoice = invoice
+        self.index = index
+    }
+
+    override func start() -> Observable<Void> {
+        let viewController = InvoiceKeypadViewController()
+        let viewModel = InvoiceKeypadViewModel(dataManager: dependencies.dataManager, for: invoice, atIndex: index)
+        viewController.viewModel = viewModel
+
+        let presentedViewController: UIViewController & ModalKeypadDismissing
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            presentedViewController = viewController
+        case .pad:
+            /// TODO: use rootViewController dimensions to configure modalViewController constraints
+            presentedViewController = ModalKeypadViewController(keypadViewController: viewController)
+        default:
+            fatalError("Unable to setup bindings for unrecognized device: \(UIDevice.current.userInterfaceIdiom)")
+        }
+
+        rootViewController.present(presentedViewController, animated: true)
+
+        return presentedViewController.dismissalEvents
+            .take(1)
+            .do(onNext: { [weak self] _ in self?.rootViewController.dismiss(animated: true) })
+    }
+
+}
